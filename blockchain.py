d5e7f9eaff04a2dd1497dce105a1e1daf7b06eee
Pavithra <pavithra.rajan01@gmail.com>
2023-05-02 23:05:07 +0530

    Completed add_node functionality
    - implemented the rewarding and penalizing of nodes
    - fixed the add_node to adhere to the design
    - removed additional node attribute to maintain authority index as it is redundant
    - added vote logic to take care of all cases and reject 50:50 case

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,11 @@
 import hashlib
 import json
-from web3 import Web3
 import datetime as _dt
 import requests
 import argparse
 import logging
 import datetime
 import os
-import ast
 
 # Set the logging level to debug
 class ColourLogs(logging.Formatter):
@@ -83,13 +81,14 @@ class Block:
 nodes = {}
 authority_nodes = {}
 authority_count = 0
-candidate_count = 0
+follower_count = 0
 current_primary = None
 AUTHORITY_THRESHOLD = 1000
 primary_index = 0
 BLOCK_REWARD = 100
-PENALTY = 250
-MIN_TRANSACTION_RATIO = 2
+PRIMARY_REWARD = 250
+PENALTY = 450
+MAX_TRANSACTION_RATIO = 3
 VERIFY_URL = "http://127.0.0.1:5000/verify-certificate"
 CA_CHAIN_URL = "http://127.0.0.1:8200/v1/pki/ca_chain"
 
@@ -98,80 +97,103 @@ class Node:
         self.is_authority = False
         self.is_full_node = False
         self.reputation = 0
-        self.authority_index = -1
         self.certificate = bytes()
         self.device_id = 0b0 * 160
+        self.promote_count = 0
     
     def display_node(obj):
         node_json = json.dumps(obj.__dict__, default=dict)
         return node_json
     
-    def add_node(self, nodes, candidate_count,  is_full_node, certificate, device_id, authority_result):
+    def add_node(self, nodes, follower_count,  is_full_node, certificate, device_id, authority_nodes, primary_index):
         if self.reputation != 0:
             raise Exception("Node already added")
+        
         self.is_full_node = is_full_node
         self.reputation = AUTHORITY_THRESHOLD
         self.certificate = certificate
         self.device_id = device_id
-        candidate_count = candidate_count + 1
+        if is_full_node:
+            follower_count = follower_count + 1
+
+        # These lists store the indices if they voted
+        follower_node_indices = []
+        auth_vote, authority_node_indices = authority_voting(nodes, primary_index, authority_nodes)
         
-        votes = 0
-        node_indices = []
+        if auth_vote == None:
+            return None
+
         for node_id, node_data in nodes.items():
 
-            if node_data["is_full_node"]:
+            if node_data["is_full_node"] and node_data["is_authority"] == False:
                 chain_response = requests.get(CA_CHAIN_URL)
                 logger.info(f"Node {node_id} has retrieved the CA chain")
                 # print(chain_response.text)
                 verify_response = requests.post(VERIFY_URL, data={"certificate": cert_data, "trusted": chain_response.text})
 
                 # Check if the response was successful
-                if verify_response.text == "True":
-                    logger.info(f"Node {node_id} has verified the certificate. Response: The certificate is valid")
-                    votes+=1
-                    if authority_result == verify_response.text:
-                        logger.debug(f"Node {node_id} result in consensus with authority node") 
-                        node_indices.append(node_id)
+                if str(auth_vote) == verify_response.text:
+                    logger.info(f"Node {node_id} is in consensus with authority nodes")
+                    follower_node_indices.append(node_id)
                 else:
-                    logger.warning(f"Node {node_id} has verified the certificate. Response: The certificate is invalid")
+                    logger.warning(f"Node {node_id} is not in consensus with authority nodes")
 
-            else:
+            elif node_data["is_full_node"] == False:
                 logger.warning(f"Node {node_id} could not verify due to timeout. Not full node.")
 
-        if self.check_votes(votes, nodes) and len(node_indices) != 0:
+        if auth_vote == True :
             # print(type(json.loads(self.display_node())))
             nodes[len(nodes)] = json.loads(self.display_node())
             logger.info(f"Device with device ID {device_id} added to the network")
-            self.reward_nodes(nodes, node_indices)
+            
         else:
             logger.info("Node cannot be added as majority vote not attained")
 
-    def check_votes(self, votes, nodes):
-        if votes > len(nodes)//2:
+        
+
+        self.penalize_authority(nodes, authority_node_indices)
+        self.reward_follower_nodes(nodes, follower_node_indices)
+        
+        self.update_reputation_by_authority_index(nodes, primary_index)
+        # logger.debug(authority_node_indices)
+
+    def check_votes(self, votes, all_auth_nodes):
+        if votes > len(all_auth_nodes)//2:
             return True
         return False
     
-    def reward_nodes(self, nodes, indices):
+    def reward_follower_nodes(self, nodes, indices):
         for index in indices:
             # logger.debug((nodes[index])['reputation'])
             nodes[index]['reputation'] += BLOCK_REWARD
-        logger.debug("The nodes that are in consensus with the Primary node have been rewarded")
-    
-    def penalize_primary(nodes, index):
-        nodes[index]['reputation'] = nodes[index]['reputation'] - PENALTY
-        logger.info("The Authority node has been penalized")
-        if nodes[index]['reputation'] < AUTHORITY_THRESHOLD :
-            nodes.pop(index)
-            logger.info("The Authority Node has fell below the threshold and has been removed from the network")
-
+            if nodes[index]['reputation'] >= AUTHORITY_THRESHOLD:
+                if nodes[index]['promote_count'] < MAX_TRANSACTION_RATIO:
+                    nodes[index]['is_authority'] =  True
+                    nodes[index]['promote_count'] += 1
+                    logger.info(f"Node {index} has been promoted to Authority node")
+                else:
+                    logger.warning(f"Node {index} has crossed threshold but has been promoted for more than maximum transaction limit")
         
-def get_authority():
+        logger.info("The nodes that are in consensus with the Authority nodes have been rewarded")
+    
+    def update_reputation_by_authority_index(self, nodes, authority_index):
+        for node_id, node_data in nodes.items():
+            if node_id == authority_index:
+                node_data["reputation"] += PRIMARY_REWARD
+
+    def penalize_authority(self, nodes, voted_indices):
+        for node_id, node_data in nodes.items():
+            if node_data["is_authority"] and node_id not in voted_indices:
+                node_data["reputation"] -= PENALTY
+                logger.info(f"Node {node_id} is an Authority node and has been penalized: Reason: No vote in transaction")
+        
+def get_primary():
     if not os.path.exists('index.txt'):
         return 0
     with open('index.txt', 'r') as f:
         return int(f.read().strip())
     
-def set_authority(index):
+def set_primary(index):
     with open('index.txt', 'w') as f:
         f.write(str(index))
 
@@ -184,6 +206,50 @@ def authority_verify(index, cert_data):
     # logger.info(verify_response.text)
     return verify_response.text
 
+def get_authority_device_ids(nodes):
+    return [node_id for node_id, node_data in nodes.items() if node_data["is_authority"]]
+
+
+def penalize_primary(nodes, index, follower_count):
+        nodes[index]['reputation'] = nodes[index]['reputation'] - PENALTY
+        logger.info("The Authority node has been penalized")
+        if nodes[index]['reputation'] < AUTHORITY_THRESHOLD :
+            nodes.pop(index)
+            logger.info("The Authority Node has fell below the threshold and has been removed from the network")
+            follower_count -= 1
+
+def authority_voting(nodes, primary_index, authority_nodes):
+    print(primary_index)
+    authority_node_votes = {}
+    votes_true = 0
+    votes_false = 0
+    logger.info("Verification and voting by authority nodes have begun")
+    for node_id, node_data in nodes.items():
+
+        if node_data["is_authority"]:
+            chain_response = requests.get(CA_CHAIN_URL)
+            logger.info(f"Node {node_id} has retrieved the CA chain")
+            # print(chain_response.text)
+            verify_response = requests.post(VERIFY_URL, data={"certificate": cert_data, "trusted": chain_response.text})
+
+            # Check if the response was successful
+            if verify_response.status_code == 200:
+                if verify_response.text == "True":
+                    logger.info(f"Node {node_id} has verified the certificate. Response: The certificate is valid")
+                    votes_true += 1
+                    authority_node_votes[node_id] = "True"
+                else:
+                    logger.warning(f"Node {node_id} has verified the certificate. Response: The certificate is invalid")
+                    votes_false += 1
+                    authority_node_votes[node_id] = "False"
+            else:
+                authority_node_votes[node_id] = "Fail"
+    
+    if votes_true == votes_false >= len(authority_node_votes) // 2:
+        return None, []
+    elif votes_true > votes_false or votes_false > votes_true:
+        return votes_true > len(authority_node_votes) // 2, authority_node_votes.keys()
+    return None, []           
 
 genesis_block = Block(0,  str(_dt.datetime.now()), "Genesis Block",0)
 
@@ -211,12 +277,13 @@ if __name__ == '__main__':
     method = args.method
 
     nodes = {
-            0: {"is_authority": False, "is_full_node": True, "reputation": 1000, "authority_index": -1, "certificate": "cert", "device_id": 49},
-            1: {"is_authority": True, "is_full_node": False, "reputation": 500, "authority_index": 0, "certificate": "cert", "device_id": 44},
-            2: {"is_authority": False, "is_full_node": True, "reputation": 750, "authority_index": -1, "certificate": "cert", "device_id": 45},
-            3: {"is_authority": True, "is_full_node": True, "reputation": 1500, "authority_index": 1, "certificate": "cert", "device_id": 54},
+            0: {"is_authority": False, "is_full_node": True, "reputation": 900, "certificate": "cert", "device_id": 49, "promote_count": 1},
+            1: {"is_authority": True, "is_full_node": True, "reputation": 1200, "certificate": "cert", "device_id": 44, "promote_count": 1},
+            2: {"is_authority": False, "is_full_node": True, "reputation": 750, "certificate": "cert", "device_id": 45, "promote_count": 0},
+            3: {"is_authority": True, "is_full_node": True, "reputation": 1500, "certificate": "cert", "device_id": 54, "promote_count": 1},
+            4: {"is_authority": False, "is_full_node": False, "reputation": 50, "certificate": "cert", "device_id": 79, "promote_count": 0},
             }
-
+    
     if method == 'add':
 
         certificate_path = args.certificate        
@@ -233,15 +300,16 @@ if __name__ == '__main__':
                 cert_data = cert_file.read()
 
         node = Node()
-                
-        authority_nodes = [44, 54]
-        primary_index = get_authority() % len(authority_nodes)
+        logger.debug(f"Authority node device IDs {get_authority_device_ids(nodes)}")
+        authority_nodes = get_authority_device_ids(nodes)
+        print(authority_nodes)
+        primary_index = get_primary() % len(authority_nodes)
         # print(primary_index)
-        set_authority(primary_index + 1)
+        set_primary(primary_index + 1)
         authority_result = authority_verify(primary_index, cert_data)
         
         try:
-            node.add_node(nodes, candidate_count, full_node, cert_data, device_id, authority_result)
+            node.add_node(nodes, follower_count, full_node, cert_data, device_id, authority_nodes, primary_index)
             # logger.info(node.display_node())
         except Exception as e:
             logger.error(e)
@@ -249,5 +317,5 @@ if __name__ == '__main__':
     elif method == 'viewnode':
         node_num = args.node
         logger.info(json.dumps(nodes[node_num], indent=4))
-    # print(nodes)
+    # logger.debug(json.dumps(nodes, indent=4))
     # print(block_hashes)
